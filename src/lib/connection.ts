b3bc090a545ec59e04665a11677f4d255c03e527
Luis David Santiago Santana <luisdavidsantiagosantana@gmail.com>
2020-09-25 02:40:55 -0400

    feat: create connection file

---
+++ src/lib/connection.ts
@@ -0,0 +1,21 @@
+import { Sequelize } from 'sequelize';
+
+export const sequelize = new Sequelize({
+	dialect: 'mysql',
+	dialectOptions: {
+		dateStrings: true,
+		typeCast: true,
+		useUTC: false
+	},
+	define: {
+		charset: 'utf8',
+		collate: 'utf8_general_ci'
+	},
+	timezone: '-04:00',
+	host: process.env.DB_HOST,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	port: Number(process.env.DB_PORT),
+	database: process.env.DB_NAME,
+	logging: false
+});
