ab58c41e13c9e34f63a835a1f25215e42c0c2a75
Luis David Santiago Santana <luisdavidsantiagosantana@gmail.com>
2020-09-28 02:52:20 -0400

    feat: ticket create

--- src/components/tickets/controller.ts
+++ src/components/tickets/controller.ts
@@ -1,9 +1,25 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
+import axios from 'axios';
 
-import { Ticket } from './model'
+import { Ticket, TicketProduct } from './model';
+const API_URL = process.env.API_URL;
 
 export default {
 	create: (req: Request, res: Response) => {
+		const { ticket, products } = req.body;
+		ticket.userId = req.session!.user.id;
 
+		Ticket.create(
+			{ ...ticket, TicketProducts: products },
+			{ include: [TicketProduct], raw: true }
+		)
+			.then((results) => {
+				res.sendStatus(201);
+				//axios.post(API_URL + '/')
+			})
+			.catch((error) => {
+				res.sendStatus(500);
+				throw error;
+			});
 	}
-}
+};
--- src/components/tickets/model.ts
+++ src/components/tickets/model.ts
@@ -1,37 +1,60 @@
-import { DataTypes } from 'sequelize'
+import { DataTypes } from 'sequelize';
 
-import { sequelize } from '../../lib/connection'
-import { User } from '../users/model'
-import { Shift } from '../shifts/model'
-import { Product } from '../products/model'
+import { sequelize } from '../../lib/connection';
+import { User } from '../users/model';
+import { Shift } from '../shifts/model';
+import { Product } from '../products/model';
 
-const Ticket = sequelize.define('Ticket', {
-	id: {
-		type: DataTypes.UUID,
-		defaultValue: DataTypes.UUIDV4,
-		primaryKey: true,
-		allowNull: false,
-		unique: true
+const Ticket = sequelize.define(
+	'Ticket',
+	{
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			primaryKey: true,
+			allowNull: false,
+			unique: true
+		},
+		ticketNumber: {
+			type: DataTypes.BIGINT,
+			allowNull: true
+		},
+		amount: {
+			type: DataTypes.FLOAT,
+			allowNull: false
+		},
+		status: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				isIn: [['IN PROCESS', 'DONE', 'PAUSED', 'CANCELLED']]
+			}
+		}
 	},
-	ticketNumber: {
-		type: DataTypes.BIGINT,
-		autoIncrement: true,
-		allowNull: false
-	},
-	amount: {
-		type: DataTypes.FLOAT,
-		allowNull: false
-	},
-	status: {
-		type: DataTypes.STRING,
-		allowNull: false,
-		validate: {
-			isIn: [['IN PROCESS', 'DONE', 'PAUSED', 'CANCELLED']]
+	{
+		hooks: {
+			beforeCreate: async (model: any) => {
+				try {
+					const max = await Ticket.max('ticketNumber');
+					const last = Number(max) | 0;
+					model.ticketNumber = last + 1;
+				} catch (error) {
+					throw error;
+				}
+			}
 		}
 	}
-})
+);
 
 const TicketProduct = sequelize.define('TicketProduct', {
+	ticketId: {
+		type: DataTypes.UUID,
+		allowNull: false
+	},
+	productId: {
+		type: DataTypes.INTEGER,
+		allowNull: false
+	},
 	quantity: {
 		type: DataTypes.TINYINT,
 		allowNull: false,
@@ -41,16 +64,13 @@ const TicketProduct = sequelize.define('TicketProduct', {
 		type: DataTypes.FLOAT,
 		allowNull: false
 	}
-})
+});
 
-Ticket.belongsTo(User)
-Ticket.belongsTo(Shift)
-TicketProduct.hasOne(Product)
+TicketProduct.removeAttribute('id');
 
-Ticket.sync({ force: false })
-TicketProduct.sync({ force: false })
+Ticket.belongsTo(User, { foreignKey: 'userId' });
+Ticket.belongsTo(Shift, { foreignKey: 'shiftId' });
+Ticket.hasMany(TicketProduct, { foreignKey: 'ticketId' });
+TicketProduct.belongsTo(Product, { foreignKey: 'productId' });
 
-export {
-	Ticket,
-	TicketProduct
-}
+export { Ticket, TicketProduct };
