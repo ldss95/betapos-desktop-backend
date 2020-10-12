e25f428edfb2421db259bb1ba45abb45b0acc53b
Luis David Santiago Santana <luisdavidsantiagosantana@gmail.com>
2020-10-11 23:31:03 -0400

    refactor: remove sync code and delegate to another file

--- src/components/tickets/controller.ts
+++ src/components/tickets/controller.ts
@@ -1,46 +1,9 @@
 import { Request, Response } from 'express';
-import axios from 'axios';
-import http from 'http';
 
 import { Product } from '../products/model';
 import { Ticket } from './model';
 import { TicketProduct } from '../ticket-product/model';
-const API_URL = process.env.API_URL;
-
-function sendToAPI(ticket: any, shop: any) {
-	ticket = JSON.parse(JSON.stringify(ticket));
-
-	ticket.ticketNumber = `D-${
-		shop.prefix
-	}-${ticket.ticketNumber.toString().padStart(8, '0')}`;
-	ticket.shopId = shop.id;
-	const strTicket = JSON.stringify({ ticket });
-
-	const options = {
-		hostname: 'localhost',
-		port: 4012,
-		path: '/tickets',
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	};
-
-	const req = http.request(options, (res) => {
-		console.log(`statusCode: ${res.statusCode}`);
-
-		res.on('data', (d) => {
-			process.stdout.write(d);
-		});
-	});
-
-	req.on('error', (error) => {
-		console.error(error);
-	});
-
-	req.write(strTicket);
-	req.end();
-}
+import { sendToAPI } from '../sync/controller'
 
 export default {
 	create: async (req: Request, res: Response) => {
@@ -48,11 +11,28 @@ export default {
 			const { ticket, products, shop } = req.body;
 			ticket.userId = req.session!.user.id;
 
-			const savedTicket = await Ticket.create(
-				{ ...ticket, TicketProducts: products },
-				{ include: [TicketProduct], raw: true }
-			);
-			sendToAPI(savedTicket, shop);
+			const results = await Ticket.create({
+				...ticket,
+				TicketProducts: products
+			}, {
+				include: [TicketProduct],
+				
+			});
+
+			const savedTicket = results.get({ plain: true })
+			const prefix = 'D-' + shop.prefix
+			const ticketNumberStr = savedTicket.ticketNumber.toString()
+			const ticketNumberPaded = ticketNumberStr.padStart(8, '0')
+			savedTicket.ticketNumber = `${prefix}-${ticketNumberPaded}`;
+			savedTicket.shopId = shop.id
+
+			sendToAPI({
+				path: '/tickets', data: { ticket: savedTicket },
+				attemp: 1,
+				isNew: true,
+				callback: () => {}
+			});
+			
 			res.sendStatus(201);
 		} catch (error) {
 			res.sendStatus(500);
@@ -60,28 +40,21 @@ export default {
 		}
 	},
 	getOne: async (req: Request, res: Response) => {
-		try {
-			const { ticketNumber } = req.params;
+		const { ticketNumber } = req.params;
 
-			const ticket = await Ticket.findOne({
-				raw: true,
-				where: { ticketNumber }
+		Ticket.findOne({
+			where: { ticketNumber },
+			include: [
+				{
+					model: TicketProduct,
+					include: [Product]
+				}
+			]
+		})
+			.then((ticket) => res.status(200).send(ticket))
+			.catch((error) => {
+				res.sendStatus(500);
+				throw error;
 			});
-
-			if (!ticket) {
-				res.sendStatus(204);
-				return;
-			}
-
-			ticket.TicketProducts = await TicketProduct.findAll({
-				where: { ticketId: ticket!.id },
-				include: [Product]
-			});
-
-			res.status(200).send(ticket);
-		} catch (error) {
-			res.sendStatus(500);
-			throw error;
-		}
 	}
 };
