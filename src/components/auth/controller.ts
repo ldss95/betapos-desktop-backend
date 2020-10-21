ea86f91aff77df32653b0a35e3e7355b5fd8ebe2
Luis David Santiago Santana <luisdavidsantiagosantana@gmail.com>
2020-10-20 23:32:24 -0400

    gitflow-feature-stash: save-shopid-on-session

--- src/components/auth/controller.ts
+++ src/components/auth/controller.ts
@@ -3,69 +3,75 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 import { User } from '../users/model';
+import { Meta } from '../meta/model'
 
 export default {
-	login: (req: Request, res: Response) => {
-		const { nickName, password } = req.body;
+	login: async (req: Request, res: Response) => {
+		try {
+			const { nickName, password } = req.body;
 
-		User.findOne({
-			raw: true,
-			attributes: ['id', 'name', 'password', 'role', 'isActive'],
-			where: { nickName }
-		})
-			.then((user: any) => {
-				if (!user) {
-					res.status(401).send({
-						error: 'Nick',
-						message: 'El usuario ingresao no existe.'
-					});
-					return;
-				}
-
-				if (!user.isActive) {
-					res.status(401).send({
-						error: 'User',
-						message: `El usuario ${nickName} ha sido desabilitado por la administracion.`
-					});
-					return;
-				}
-
-				if (!bcrypt.compareSync(password, user.password!)) {
-					res.status(401).send({
-						error: 'Password',
-						message: 'Contraseña incorrecta.'
-					});
-					return;
-				}
-
-				delete user.password;
-
-				const payload = {
-					iss: '20&10-Caja',
-					aud: 'web',
-					iat: new Date().getTime() / 1000,
-					user: [user.id, user.name, user.nickName, user.role]
-				};
-
-				const token = jwt.sign(payload, `${process.env.SECRET_TOKEN}`, {
-					expiresIn: '24h'
-				});
-				req.session!.user = { ...user, token };
-				res.status(200).send({
-					isLoggedIn: true,
-					token: token,
-					nickName,
-					name: user.name,
-					role: user.role,
-					phone: user.phone,
-					dui: user.dui,
-					id: user.id
-				});
+			const user: any = await User.findOne({
+				raw: true,
+				attributes: ['id', 'name', 'password', 'role', 'isActive'],
+				where: { nickName }
 			})
-			.catch((error) => {
-				res.sendStatus(500);
-				throw error;
+
+			if (!user) {
+				res.status(401).send({
+					error: 'Nick',
+					message: 'El usuario ingresao no existe.'
+				});
+				return;
+			}
+
+			if (!user.isActive) {
+				res.status(401).send({
+					error: 'User',
+					message: `El usuario ${nickName} ha sido desabilitado por la administracion.`
+				});
+				return;
+			}
+
+			if (!bcrypt.compareSync(password, user.password!)) {
+				res.status(401).send({
+					error: 'Password',
+					message: 'Contraseña incorrecta.'
+				});
+				return;
+			}
+
+
+			delete user.password;
+
+			const payload = {
+				iss: '20&10-Caja',
+				aud: 'web',
+				iat: new Date().getTime() / 1000,
+				user: [user.id, user.name, user.nickName, user.role]
+			};
+
+			const token = jwt.sign(payload, `${process.env.SECRET_TOKEN}`, {
+				expiresIn: '24h'
 			});
+
+			req.session!.user = { ...user, token };
+			const meta = await Meta.findOne()
+			req.session!.shopId = meta?.shopId
+
+			res.status(200).send({
+				isLoggedIn: true,
+				token: token,
+				nickName,
+				name: user.name,
+				role: user.role,
+				phone: user.phone,
+				dui: user.dui,
+				id: user.id
+			});
+		} catch (error) {
+			res.sendStatus(500);
+			throw error;
+		}
 	},
 	logout: (req: Request, res: Response) => {
 		req.session!.destroy((error) => {
