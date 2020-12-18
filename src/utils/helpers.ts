2b0bee6f9df5c51200b15bb5d3476c69e4caa600
Luis David Santiago Santana <luisdavidsantiagosantana@gmail.com>
2020-12-18 07:57:02 -0400

    feat add upload file helper and attach email href

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,6 @@
 import nodemailer from 'nodemailer'
+import { ReadStream } from 'fs'
+import aws from 'aws-sdk'
 
 const transporter = nodemailer.createTransport({
 	secure: false,
@@ -21,7 +23,8 @@ interface msgAttr {
 	html: string;
 	attachments?: {
 		filename: string;
-		path: string;
+		path?: string;
+		href?: string; 
 	}[];
 }
 
@@ -35,4 +38,36 @@ function sendMessage(msg: msgAttr) {
 	})
 }
 
-export { sendMessage };
+/**
+ * Asynchronously upload file to Digital Ocean Spaces
+ * @param path A path to upload file on Digital Ocean Space
+ * @param fileName A name of file
+ * @param content A content to uplod, can be file, or stream
+ * @param type type of file, like application/pdf
+ * 
+ * @return string url
+ */
+function uploadFile(path: string, fileName: string, content: ReadStream | File, type: 'application/pdf' | 'application/excel') {
+	const spaceEndpoint = new aws.Endpoint(process.env.S3_ENDPOINT!)
+	const s3 = new aws.S3({ endpoint: spaceEndpoint })
+	
+	const uploadParams = {
+		Key: `${path}${fileName}`,
+		Body: content,
+		Bucket: `${process.env.BUCKET_NAME}`,
+		ContentType: type,
+		ACL: 'public-read'
+	}
+
+	return new Promise<string>((resolve, reject) => {
+		s3.upload(uploadParams, (error: any, results: any) => {
+			if (error) {
+				return reject(error)
+			}
+			
+			resolve(results.Location)
+		})
+	})
+}
+
+export { sendMessage, uploadFile };
