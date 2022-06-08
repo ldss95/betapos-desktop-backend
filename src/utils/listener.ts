f22fccab1b951250e29d1f1d885f4b14158500ef
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-06-08 08:18:03 -0400

    feat: agrega listener para los datos de la empresa

--- src/utils/listener.ts
+++ src/utils/listener.ts
@@ -6,6 +6,7 @@ import { db } from '../db/firebase'
 import { Product, Barcode } from '../components/products/model'
 import { User } from '../components/users/model'
 import { Update } from '../components/update/model'
+import { Business } from '../components/business/model'
 
 const {
 	API_URL,
@@ -15,214 +16,291 @@ const {
 let unsubscribers: any = []
 
 async function listen() {
-		for(const unsubscribe of unsubscribers) {
-			unsubscribe()
-		}
+	if (!MERCHANT_ID) {
+		return console.error('No se puede sincronizar porque no se encontró el MERCHANT ID en las variables de entorno')
+	}
+
+	for(const unsubscribe of unsubscribers) {
+		unsubscribe()
+	}
 		
-		const productsRef = doc(db, MERCHANT_ID!, 'products')
-		const productsUnsubscriber = onSnapshot(productsRef, async (doc) => {
-			try {
-				const data = doc.data()
-				if (!data) {
-					return;
-				}
+	const productsRef = doc(db, MERCHANT_ID, 'products')
+	const productsUnsubscriber = onSnapshot(productsRef, async (doc) => {
+		try {
+			const data = doc.data()
+			if (!data) {
+				return;
+			}
 
-				// Productos eliminados
-				const { deleted, lastUpdate } = data;
-				if (deleted && deleted.length > 0) {
-					Product.destroy({
-						where: {
-							id: {
-								[Op.in]: deleted
-							}
-						}
-					})
-				}
-
-				const update = await Update.findOne({
-					where: { table: 'products' }
-				})
-
-				if(!update){
-					await Update.create({ table: 'products' })
-				}
-
-				if(update?.date == lastUpdate){
-					return;
-				}
-
-				const { data: { created, updated } } = await axios.get(
-					`${API_URL}/products/updates/${update?.date || 'ALL'}`,
-					{
-						headers: {
-							merchantId: MERCHANT_ID!
-						}
-					}
-				);
-
-				// Productos modificados
-				for(const product of updated){
-					await Product.update(product, {
-						where: {
-							id: product.id
-						}
-					})
-				}
-
-				// Productos nuevos
-				await Product.bulkCreate(created, {
-					ignoreDuplicates: true
-				});
-
-				// Codigos de barra nuevos
-				await Barcode.bulkCreate(created.map(({ barcodes }: any) => barcodes).flat(), {
-					ignoreDuplicates: true
-				})
-
-				await Update.update({ date: lastUpdate }, {
+			// Productos eliminados
+			const { deleted, lastUpdate } = data;
+			if (deleted && deleted.length > 0) {
+				Product.destroy({
 					where: {
-						table: 'products'
+						id: {
+							[Op.in]: deleted
+						}
 					}
 				})
-			} catch (error) {
-				console.log(error)
-				// throw error;
 			}
-		}, (error: any) => {
-			throw error
-		})
 
-		unsubscribers.push(productsUnsubscriber)
+			const update = await Update.findOne({
+				where: { table: 'products' }
+			})
 
-		const barcodesRef = doc(db, MERCHANT_ID!, 'barcodes')
-		const barcodesUnsubscribers = onSnapshot(barcodesRef, async (doc) => {
-			try {
-				const data: any = doc.data();
-				if (!data) {
-					return;
+			if(!update){
+				await Update.create({ table: 'products' })
+			}
+
+			if(update?.date == lastUpdate){
+				return;
+			}
+
+			const { data: { created, updated } } = await axios.get(
+				`${API_URL}/products/updates/${update?.date || 'ALL'}`,
+				{
+					headers: {
+						merchantId: MERCHANT_ID!
+					}
 				}
+			);
 
-				// Barcodes eliminados
-				const { deleted, lastUpdate } = data;
-				if (deleted && deleted.length > 0) {
-					Barcode.destroy({
-						where: {
-							id: {
-								[Op.in]: deleted
-							}
-						}
-					})
-				}
-
-				const update = await Update.findOne({
-					where: { table: 'barcodes' }
+			// Productos modificados
+			for(const product of updated){
+				await Product.update(product, {
+					where: {
+						id: product.id
+					}
 				})
+			}
 
-				if(!update){
-					await Update.create({ table: 'barcodes' })
+			// Productos nuevos
+			await Product.bulkCreate(created, {
+				ignoreDuplicates: true
+			});
+
+			// Codigos de barra nuevos
+			await Barcode.bulkCreate(created.map(({ barcodes }: any) => barcodes).flat(), {
+				ignoreDuplicates: true
+			})
+
+			await Update.update({ date: lastUpdate }, {
+				where: {
+					table: 'products'
 				}
+			})
+		} catch (error) {
+			console.log(error)
+			// throw error;
+		}
+	}, (error: any) => {
+		throw error
+	})
 
-				if(update?.date == lastUpdate){
-					return;
+	unsubscribers.push(productsUnsubscriber)
+
+	const barcodesRef = doc(db, MERCHANT_ID, 'barcodes')
+	const barcodesUnsubscribers = onSnapshot(barcodesRef, async (doc) => {
+		try {
+			const data: any = doc.data();
+			if (!data) {
+				return;
+			}
+
+			// Barcodes eliminados
+			const { deleted, lastUpdate } = data;
+			if (deleted && deleted.length > 0) {
+				Barcode.destroy({
+					where: {
+						id: {
+							[Op.in]: deleted
+						}
+					}
+				})
+			}
+
+			const update = await Update.findOne({
+				where: { table: 'barcodes' }
+			})
+
+			if(!update){
+				await Update.create({ table: 'barcodes' })
+			}
+
+			if(update?.date == lastUpdate){
+				return;
+			}
+
+			const { data: { created, updated } } = await axios.get(
+				`${API_URL}/barcodes/updates/${update?.date || 'ALL'}`,
+				{
+					headers: {
+						merchantId: MERCHANT_ID!
+					}
 				}
+			);
 
-				const { data: { created, updated } } = await axios.get(
-					`${API_URL}/barcodes/updates/${update?.date || 'ALL'}`,
+			// Barcodes modificados
+			for(const barcode of updated){
+				await Barcode.update(barcode, {
+					where: {
+						id: barcode.id
+					}
+				})
+			}
+
+			// Barcodes nuevos
+			await Barcode.bulkCreate(created, { ignoreDuplicates: true });
+
+			await Update.update({ date: lastUpdate }, {
+				where: { table: 'barcodes' }
+			})
+		} catch (error) {
+			throw error;
+		}
+	}, (error: any) => {
+		throw error
+	})
+
+	unsubscribers.push(barcodesUnsubscribers)
+
+	const usersRef = doc(db, MERCHANT_ID, 'users')
+	const usersUnsubscribers = onSnapshot(usersRef, async (doc) => {
+		try {
+			const data: any = doc.data();
+			if (!data) {
+				return;
+			}
+
+			// Usuarios eliminados
+			const { deleted, lastUpdate } = data;
+			if (deleted && deleted.length > 0) {
+				User.destroy({
+					where: {
+						id: {
+							[Op.in]: deleted
+						}
+					}
+				})
+			}
+
+			const update = await Update.findOne({
+				where: { table: 'users' }
+			})
+
+			if(!update){
+				await Update.create({ table: 'users' })
+			}
+
+			if(update?.date == lastUpdate){
+				return;
+			}
+
+			const { data: { created, updated } } = await axios.get(
+				`${API_URL}/users/updates/${update?.date || 'ALL'}`,
+				{
+					headers: {
+						merchantId: MERCHANT_ID!
+					}
+				}
+			);
+
+			// Usuarios modificados
+			for(const user of updated){
+				await User.update(user, {
+					where: {
+						id: user.id
+					}
+				})
+			}
+
+			// Usuarios nuevos
+			await User.bulkCreate(created, { ignoreDuplicates: true });
+
+			await Update.update({ date: lastUpdate }, {
+				where: { table: 'users' }
+			})
+		} catch (error) {
+			throw error;
+		}
+	}, (error: any) => {
+		throw error
+	})
+
+	unsubscribers.push(usersUnsubscribers)
+
+	const businessRef = doc(db, MERCHANT_ID, 'business')
+	const businessUnsubscribers = onSnapshot(businessRef, async (doc) => {
+		try {
+			const data: any = doc.data();
+			if (!data) {
+				return;
+			}
+
+			const { lastUpdate } = data;
+			const business = await Business.findOne();
+			const res = await axios.get(
+				`${API_URL}/business/by-merchant-id`,
+				{
+					headers: {
+						merchantId: MERCHANT_ID
+					}
+				}
+			);
+
+			if(!res.data) {
+				return console.error('Empresa no encontrada, MERCHANT ID incorrecto!');
+			}
+
+			if (!business) {
+				await Business.create(res.data);
+
+				await Update.bulkCreate([
 					{
-						headers: {
-							merchantId: MERCHANT_ID!
-						}
-					}
-				);
-
-				// Barcodes modificados
-				for(const barcode of updated){
-					await Barcode.update(barcode, {
-						where: {
-							id: barcode.id
-						}
-					})
-				}
-
-				// Barcodes nuevos
-				await Barcode.bulkCreate(created, { ignoreDuplicates: true });
-
-				await Update.update({ date: lastUpdate }, {
-					where: { table: 'barcodes' }
-				})
-			} catch (error) {
-				throw error;
-			}
-		}, (error: any) => {
-			throw error
-		})
-
-		unsubscribers.push(barcodesUnsubscribers)
-
-		const usersRef = doc(db, MERCHANT_ID!, 'users')
-		const usersUnsubscribers = onSnapshot(usersRef, async (doc) => {
-			try {
-				const data: any = doc.data();
-				if (!data) {
-					return;
-				}
-
-				// Usuarios eliminados
-				const { deleted, lastUpdate } = data;
-				if (deleted && deleted.length > 0) {
-					User.destroy({
-						where: {
-							id: {
-								[Op.in]: deleted
-							}
-						}
-					})
-				}
-
-				const update = await Update.findOne({
-					where: { table: 'users' }
-				})
-
-				if(!update){
-					await Update.create({ table: 'users' })
-				}
-
-				if(update?.date == lastUpdate){
-					return;
-				}
-
-				const { data: { created, updated } } = await axios.get(
-					`${API_URL}/users/updates/${update?.date || 'ALL'}`,
+						table: 'business',
+						date: lastUpdate
+					},
 					{
-						headers: {
-							merchantId: MERCHANT_ID!
-						}
-					}
-				);
+						table: 'users',
+						date: res.data.createdAt
+					},
+					{
+						table: 'products',
+						date: res.data.createdAt
+					},
+					{
+						table: 'barcodes',
+						date: res.data.createdAt
+					},
+				], { ignoreDuplicates: true })
 
-				// Usuarios modificados
-				for(const user of updated){
-					await User.update(user, {
-						where: {
-							id: user.id
-						}
-					})
-				}
-
-				// Usuarios nuevos
-				await User.bulkCreate(created, { ignoreDuplicates: true });
-
-				await Update.update({ date: lastUpdate }, {
-					where: { table: 'users' }
-				})
-			} catch (error) {
-				throw error;
+				return;
 			}
-		}, (error: any) => {
-			throw error
-		})
+			
+			const update = await Update.findOne({
+				where: { table: 'business' }
+			})
 
-		unsubscribers.push(usersUnsubscribers)
+			if(!update){
+				await Update.create({ table: 'business' });
+			}
+
+			if(update?.date == lastUpdate){
+				return;
+			}
+
+			await business.update(res.data);
+			await Update.update({ date: lastUpdate }, {
+				where: { table: 'business' }
+			})
+		} catch (error) {
+			throw error;
+		}
+	}, (error: any) => {
+		throw error
+	})
+
+	unsubscribers.push(businessUnsubscribers)
 }
 
 export { listen }
